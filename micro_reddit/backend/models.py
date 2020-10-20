740492043b8267a6a83c4d093ee5291578d43eef
Ben Steel <bsteel300@gmail.com>
2020-10-20 14:58:03 -0700

    migrations for Post model

--- micro_reddit/backend/models.py
+++ micro_reddit/backend/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from .validators import validate_password
 
 # Create your models here.
+def get_sentinel_user():
+    return User.objects.get_or_create(username='deleted')[0]
+
 class User(models.Model):
-    name = models.CharField(max_length=40, unique=True)
+    username = models.CharField(max_length=40, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length= 20, validators=[validate_password])
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, 
+        on_delete=models.SET(get_sentinel_user),
+        )
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
